d0067aca1afd3a0b9baa3b7654119fa608a83062
Andreas Georgiou <andreasgeorgiou222@gmail.com>
2025-05-27 15:20:20 +0300

    commit 2 did the menu

---
+++ FinalExam.cpp
@@ -0,0 +1,47 @@
+/**
+@brief a program which can take a range of numbers and gives you the sum of all negative
+@author andreas georgiou
+@param enters data in matrix within a range
+@param sums out all negative numbers
+@return matrix<code>positive</code>
+@return sum<code>negative
+
+*/
+//libraries
+#include<iostream>
+#include<cassert>
+ 
+// function parameters
+
+void matrix();
+void nsum();
+
+int main{
+	double matrix[3][5];
+int choice;
+
+do
+	cout << "1) Enter data in matrix (range -1.5 to 0.5)" << endl;
+	cout << " 2) Sum of all negative numbers" << end;;
+	cout << " 0) Exit " >> endl;
+	cout << " Please enter choice:"
+	cin >> choice;
+if (choice == 1) {
+	matrix();
+}
+else if (choice == 2) {
+	nsum();
+}
+else
+	cout << choice << " doesnt exist in the menu try again"<<endl;
+	cin >> choice;
+
+	while (choice != 0);
+
+	return 0;
+}
+void matrix() {
+	cin >> matrix[][];
+	
+}
+
