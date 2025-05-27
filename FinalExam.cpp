5a7a7a025bb7eab0e070741c852d7e11a8f0cde1
Andreas Georgiou <andreasgeorgiou222@gmail.com>
2025-05-27 15:53:36 +0300

    did the function of fmatrix

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -13,39 +13,54 @@
  
 // function parameters
 
-double fmatrix();
+double fmatrix(matrix[][]);
 void nsum();
 
 int main{
-	double matrix[3][5];
+
+double  matrix[3][5];
 int choice;
 
-do
+do {
 	cout << "1) Enter data in matrix (range -1.5 to 0.5)" << endl;
-	cout << " 2) Sum of all negative numbers" << end;;
+	cout << " 2) Sum of all negative numbers" << endl;
 	cout << " 0) Exit " >> endl;
 	cout << " Please enter choice:"
-	cin >> choice;
-if (choice == 1) {
-	matrix();
-}
-else if (choice == 2) {
-	nsum();
-}
-else
-	cout << choice << " doesnt exist in the menu try again"<<endl;
+		cin >> choice;
+	if (choice == 1) {
+		matrix();
+	}
+	else if (choice == 2) {
+		nsum();
+	}
+	else
+		cout << choice << " doesnt exist in the menu try again" << endl;
 	cin >> choice;
 
 	while (choice != 0);
+	
+}
 
 	return 0;
 }
-double fmatrix(matrix[3][5]) {
+// function that takes matrix[3][5] and fills it out with data 
+// prototype
+double fmatrix(matrix[][]) {
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 15; j++) {
+			cin >> matrix[i][j];
+			if (matrix[i][j] > 0.5 || matrix[i][j] < -1.5)
+				cout << "out of bounds try again " << endl;
+			cin >> matrix[i][j];
+		}
+	}
+	return matrix[i][j];
 	
-	cin >> matrix[3][5];
 	
 }
-// nsum
+// sums up all of the negative number of matrix 
+// prototype
 void nsum(matrix[][]) {
 	double sum = 0;
 	for (int i = 0; i < 3; i++) {
@@ -57,4 +72,4 @@ void nsum(matrix[][]) {
 	}
 	cout << " The negative sum of matrix is " << sum << endl;
 }
-
+// the program doesnt run it will run the next and final commit 
